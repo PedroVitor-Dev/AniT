28b66222631888056c029f6112dbf8cfdedd2295
Pedro Vitor Alves Florêncio <72324020+PedroVitor-Dev@users.noreply.github.com>
2026-09-15 21:32:19 -0300

    fix: open dashboard maximized

--- src/AniT.App/DashboardWindow.xaml.cs
+++ src/AniT.App/DashboardWindow.xaml.cs
@@ -27,7 +27,6 @@ public partial class DashboardWindow : Window
     {
         InitializeComponent();
         ResponsiveWindow.FitToWorkArea(this, 1380, 860);
-        WindowState = WindowState.Maximized;
         DataContext = this;
     }
 
